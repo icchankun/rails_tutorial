0b5d1ce31f0f684676c5cffd112d6406531f0790
icchan-webapp <issei.0157@gmail>
2022-08-11 20:58:43 +0900

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-    def create
+  def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to user_url(@user)
     else
