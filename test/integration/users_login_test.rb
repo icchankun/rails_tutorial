e8ff8bc26bde789ff957ac2336bedb4ca5e187da
icchan-webapp <issei.0157@gmail.com>
2022-08-14 13:15:18 +0900

    Implement advanced-login

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -6,7 +6,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
   end
 
-    test "login with valid email/invalid password" do
+  test "login with valid email/invalid password" do
     get login_path
     assert_template 'sessions/new'
     post login_path, params: { session: { email:    @user.email,
@@ -36,5 +36,19 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path,      count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
+  end
+
+  test "login with remembering" do
+    log_in_as(@user, remember_me: '1')
+    assert_not_empty cookies[:remember_token]
+  end
+
+  test "login without remembering" do
+    # cookieを保存してログイン
+    log_in_as(@user, remember_me: '1')
+    delete logout_path
+    # cookieを削除してログイン
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies[:remember_token]
   end
 end
